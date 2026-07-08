d1ad353b067330de6f4d8b6c70944235e650cb4b
AdryanUchoa <nunesadryan40@gmail.com>
2026-07-08 19:06:01 +0000

    feat adding celery and redis for async process

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, status
+from core.workers import extract_geometry_task
 
 app = FastAPI(
     title="FaberAI Backend",
     description="AI-powered manufacturability review API for CAD parts.",
     version="0.1.0",
 )
-
 
 @app.get("/", tags=["Health"])
 def health_check():
@@ -14,3 +14,17 @@ def health_check():
     Returns 200 OK to confirm the server is running.
     """
     return {"status": "ok", "message": "FaberAI backend is running."}
+
+@app.post("/upload/", status_code=status.HTTP_202_ACCEPTED, tags=["Upload"])
+async def upload_cad_file(file: UploadFile):
+    file_name = file.filename
+    
+    # Dispara a tarefa assíncrona
+    task = extract_geometry_task.delay(file_name)
+    
+    return {
+        "message": "File received successfully. Processing started in background.",
+        "task_id": task.id,
+        "filename": file_name,
+        "status": "processing"
+    }
