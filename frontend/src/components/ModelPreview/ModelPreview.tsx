71ec3df9f1374fc66e3f58440b76b313d289c76f
Ken0u <k6ou@uwaterloo.ca>
2026-08-10 22:50:01 -0400

    make markers smaller

--- frontend/src/components/ModelPreview/ModelPreview.tsx
+++ frontend/src/components/ModelPreview/ModelPreview.tsx
@@ -157,7 +157,7 @@ function ModelCanvas({ xRayEnabled }: ModelCanvasProps) {
   const [markerSize, setMarkerSize] = useState(0.5)
 
   function scaleMarketSizeToGeometry(size: ThreeVector3) {
-    setMarkerSize(((size.x + size.y + size.z) / 3) * 0.04)
+    setMarkerSize(((size.x + size.y + size.z) / 3) * 0.01)
   }
 
   return (
